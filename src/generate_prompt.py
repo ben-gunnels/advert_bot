7381fe5973c49c0e1b70b2ba1e0bf3df43fd6e98
miltiades-the-general <miltiadesgeneral@gmail.com>
2025-06-11 19:18:38 -0700

    Prompt tweaked

--- src/generate_prompt.py
+++ src/generate_prompt.py
@@ -3,7 +3,7 @@ __all__ = ["generate_prompt"]
 def generate_prompt(injection=""):
     dense_prompt = """
         Add the design to the model wearing a blank T-Shirt. Ensure that the design is well fitted for the shirt and that the image
-        can be used to advertise the design of the image.
+        can be used to advertise the design of the image. Keep the design moderately sized on the shirt. It should leave 
     """
 
     dense_prompt += injection
